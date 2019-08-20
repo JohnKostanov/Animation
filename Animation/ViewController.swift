cfeaff01ec8ace63cbff665050d16cf47a854e50
JohnKostanov <kostanovd@icloud.com>
2019-08-20 23:18:02 +0200

    Add animation by tap

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -10,14 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+    let initialFrame = CGRect(x: 100, y: 100, width: 100, height: 100)
+    var subview: UIView!
+    var touchCounter = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let subview = UIView(frame: frame)
+        subview = UIView(frame: initialFrame)
         subview.backgroundColor = .purple
         view.addSubview(subview)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        touchCounter += 1
+        
+        switch touchCounter {
+        case 1:
+            UIView.animate(withDuration: 1) {
+                self.subview.backgroundColor  = .orange
+            }
+        case 2:
+            UIView.animate(withDuration: 1) {
+                self.subview.backgroundColor  = .purple
+                self.subview.frame = CGRect(x: 150, y: 150, width: 200, height: 200)
+            }
+        case 3:
+            UIView.animate(withDuration: 3) {
+                self.subview.frame  = self.initialFrame
+            }
+        default:
+            touchCounter = 0
+            
+        }
     }
 
 
